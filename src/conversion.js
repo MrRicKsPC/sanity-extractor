a8c236e26745515ee594c979f5a5332a5eef8c55
MrRicKsPC <73223747+MrRicKsPC@users.noreply.github.com>
2025-03-31 10:23:33 +0200

    Update conversion.js

--- src/conversion.js
+++ src/conversion.js
@@ -150,7 +150,11 @@ function convertToHTML(jContent) {
 
     let hContent = "";
     const hStyle = document.createElement("style");
-    hStyle.textContent = "* { font-family: \"Segoe UI\", sans-serif; } [data-type=\"pt\"] { color: royalblue; } h1 { font-size: 200%; } h2 { font-size: 185%; } h3 { font-size: 170%; } h4 { font-size: 155%; } h5 { font-size: 140%; } h6 { font-size: 125%; } h1, h2, h3, h4, h5, h6 { text-align: center; }"
+    hStyle.textContent = "* { font-family: \"Segoe UI\", sans-serif; } "
+    + "[data-type=\"pt\"] { color: royalblue; } "
+    + "h1 { font-size: 200%; } h2 { font-size: 185%; } h3 { font-size: 170%; } h4 { font-size: 155%; } h5 { font-size: 140%; } h6 { font-size: 125%; } "
+    + "h1, h2, h3, h4, h5, h6 { text-align: center; } "
+    + "span[data-mark] { color: #007bff; text-decoration: underline; cursor: pointer; }"
     hContent += hStyle.outerHTML + "\n\n";
 
     const nativeStyles = ["h1", "h2", "h3", "h4", "h5", "h6", "blockquote"];
