802391ec5362bedc8049d5db57e01af888a2b6ee
MrRicKsPC <73223747+MrRicKsPC@users.noreply.github.com>
2025-04-02 15:47:50 +0200

    Update paths.js

--- src/paths.js
+++ src/paths.js
@@ -3,7 +3,7 @@ PATHS = {
     "ebpModule": ["title", "abstract", "parts"],
     "ebpModulePart": ["title", "subparts"],
     "ebpModuleSubpart": ["title", "content"],
-
+    
     // Exercises.
     "exercise": ["title", "description"],
-  }
+}
