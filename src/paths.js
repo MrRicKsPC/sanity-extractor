5a26e74124a4998773c992ae8d3a36da6a46b011
MrRicKsPC <73223747+MrRicKsPC@users.noreply.github.com>
2025-05-21 17:27:30 +0200

    Update paths.js
    Added a path for all existing content types so far (21/05/25).

--- src/paths.js
+++ src/paths.js
@@ -6,4 +6,50 @@ PATHS = {
     
     // Exercises.
     "exercise": ["title", "description", "thumbnail"],
+
+    // Clinical tests.
+    "clinicalTest": ["title", "thumbnail", "content"],
+
+    // Masterclasses.
+    "masterclass": ["title", "thumbnail", "content"],
+
+    // Webinars.
+    "webinar": ["title", "description", "thumbnail", "transcription"],
+
+    // Clinical practices.
+    "clinicalPractice": ["title", "description", "thumbnail"],
+
+    // En tendon.
+    "enTendon": ["title", "description", "thumbnail"],
+
+    // Contributors.
+    "contributor": ["profilePicture", "name", "professionDescription", "biography"],
+
+    // Program templates.
+    "protocol": ["title", "thumbnail", "thumbnail"],
+    "session": ["description", "exerciseInstances"],
+    "exerciseInstance": ["duration", "frequency", "reps", "sets", "tempo", "rest"],
+    
+    // Protocols & clusters.
+    "protocolAndCluster": ["title", "thumbnail", "content"],
+    
+    // Clinical tools.
+    "clinicalTool": ["title", "description", "thumbnail", "pdf"],
+    
+    // Academy courses.
+    "academyCourse": ["title", "thumbnail", "about", "durationText", "trainingPrerequisites", "courseObjectives", "coursePlan", "evaluationMethod", "pedagogicalMethod", "trainingSheet"],
+    
+    // Update info popups.
+    "updateInfoPopup": ["title", "content"],
+    
+    // Plans.
+    "plan": ["title", "ctaLabel", "promotionLabel", "description", "includedSections"],
+    "includedSection": ["title", "included"],
+    "includedItem": ["description"],
+    
+    // Testimonials.
+    "testimonial": ["name", "title", "image", "content"],
+    
+    // QNAs.
+    "faq": ["question", "answer"],
 }
